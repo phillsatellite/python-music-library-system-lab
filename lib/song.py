a58f907daf337b1fa5fa5f6ddcf900406ede6a78
phillsatellite <pprints12@gmail.com>
2025-12-14 21:56:54 -0500

    commit 2

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,54 @@
 class Song:
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        #Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        #Update data when song is created
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    #increment total song count 
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    #Add genre
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    #Add artists
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    #Track # of songs per genre
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    #Track # of songs per artist 
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+        pass
     pass
